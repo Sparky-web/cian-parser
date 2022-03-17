e661181326d5a91850abb42c1c0a832eee16f5f2
Vladislav <babinovvlad@gmail.com>
2022-03-17 09:37:28 +0500

    df

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import "./parser/init.js"
 import parser from "./parser/index.js"
 import strapi from "./parser/strapi.js"
 import "./parser/server.js"
-// await strapi({dir: "./dashboard"}).start()
+await strapi({dir: "./dashboard"}).start()
 
 await parser.start()
 // await parser.parseUrl((await strapi.get("links"))[0]).catch(e => console.log(e.message))
